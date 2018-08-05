73e63769b95980abc17652c34e3549aeae24682b
Nikhil Bhasin <nikhilbhasin7@gmail.com>
2018-08-05 19:00:46 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/nikhil/stormy/MainActivity.java
+++ app/src/main/java/com/example/nikhil/stormy/MainActivity.java
@@ -1,19 +1,21 @@
 package com.example.nikhil.stormy;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
 import android.graphics.drawable.Drawable;
-import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
@@ -23,6 +25,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.nikhil.stormy.databinding.ActivityMainBinding;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -38,28 +52,38 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity implements LocationListener{
-    public  static final String TAG = MainActivity.class.getSimpleName();
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private CurrentWeather currentWeather;
     private ImageView iconImageView;
-    double latitude = 37.8267;
-    double longitude = -122.4233;
+    double latitude = 51.5074;
+    double longitude = 0.1278;
     String city = "London";
-    LocationManager locationManager;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getCurrLocation();
-        //getForecast(latitude,longitude,city);
-        Log.d(TAG,"Main UI Code is running, horray!!!");
-        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
 
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        } else
+            Toast.makeText(this, "Not Connected!", Toast.LENGTH_SHORT).show();
+        getForecast(latitude, longitude, city);
 
-        }
+        Log.d(TAG, "Main UI Code is running, horray!!!");
+
     }
 
-    private void getForecast(double latitude,double longitude,String city) {
+    private void getForecast(double latitude, double longitude, String city) {
         final ActivityMainBinding binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
         TextView darkSky = (TextView) findViewById(R.id.darkSkyAttribution);
         darkSky.setMovementMethod(LinkMovementMethod.getInstance());
@@ -69,8 +93,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         String apiKey = "37551ce9206b3a6a5f65632e2e61f7c1";
 
 
-        String forecastURL = "https://api.darksky.net/forecast/"+apiKey+"/"+latitude+","+longitude;
-        if(isNetworkAvailable()) {
+        String forecastURL = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
+        if (isNetworkAvailable()) {
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder().url(forecastURL).build();
             Call call = client.newCall(request);
@@ -86,9 +110,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
                         String jsonData = response.body().string();
                         Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
-                            currentWeather=getCurrentDetails(jsonData);
+                            currentWeather = getCurrentDetails(jsonData);
 
-                            final CurrentWeather displayWeather = new CurrentWeather(currentWeather.getLocationLabel(),currentWeather.getIcon(),currentWeather.getTime(),currentWeather.getTemperature(),currentWeather.getHumidity(),currentWeather.getPrecipChance(),currentWeather.getSummary(),currentWeather.getTimeZone());
+                            final CurrentWeather displayWeather = new CurrentWeather(currentWeather.getLocationLabel(), currentWeather.getIcon(), currentWeather.getTime(), currentWeather.getTemperature(), currentWeather.getHumidity(), currentWeather.getPrecipChance(), currentWeather.getSummary(), currentWeather.getTimeZone());
                             binding.setWeather(displayWeather);
                             runOnUiThread(new Runnable() {
                                 @Override
@@ -104,18 +128,18 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
                         }
                     } catch (IOException e) {
                         Log.e(TAG, "IO Exception Caught", e);
-                    } catch (JSONException e){
-                        Log.e(TAG,"JSON Exception Caught",e);
+                    } catch (JSONException e) {
+                        Log.e(TAG, "JSON Exception Caught", e);
                     }
                 }
             });
         }
     }
 
-    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException{
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
         JSONObject forecast = new JSONObject(jsonData);
         String timezone = forecast.getString("timezone");
-        Log.i(TAG,"From JSON: " + timezone);
+        Log.i(TAG, "From JSON: " + timezone);
         JSONObject currently = forecast.getJSONObject("currently");
         CurrentWeather currentWeather = new CurrentWeather();
         currentWeather.setHumidity(currently.getDouble("humidity"));
@@ -126,7 +150,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         currentWeather.setSummary(currently.getString("summary"));
         currentWeather.setTemperature(currently.getDouble("temperature"));
         currentWeather.setTimeZone(timezone);
-        Log.d(TAG,currentWeather.getFormattedTime());
+        Log.d(TAG, currentWeather.getFormattedTime());
         return currentWeather;
     }
 
@@ -134,73 +158,193 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = manager.getActiveNetworkInfo();
         boolean isAvailable = false;
-        if(networkInfo!=null && networkInfo.isConnected())
-        {
-            isAvailable=true;
-        }
-        else
-        {
-            Toast.makeText(this, R.string.network_unavailable_message,Toast.LENGTH_LONG).show();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        } else {
+            Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
         }
         return isAvailable;
     }
 
     private void alertUserAboutError() {
         AlertDialogFragement dialog = new AlertDialogFragement();
-        dialog.show(getFragmentManager(),"error_dialog");
+        dialog.show(getFragmentManager(), "error_dialog");
     }
-    public void refreshOnClick(View view){
-        Toast.makeText(this,"Refreshing Data",Toast.LENGTH_LONG).show();
-        getCurrLocation();
-    }
-    void getCurrLocation() {
-        try {
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, (LocationListener) this);
-        }
-        catch(SecurityException e) {
-            e.printStackTrace();
-        }
-    }
-    public void onLocationChanged(Location location) {
 
+    public void refreshOnClick(View view) throws IOException {
+        Toast.makeText(this, "Refreshing Data", Toast.LENGTH_SHORT).show();
+        getLocation();
+        getForecast(latitude, longitude, city);
+    }
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
 
-        try {
-            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-            latitude=location.getLatitude();
-            longitude=location.getLongitude();
-            String city1 = addresses.get(0).getAddressLine(0);
-            Log.d(TAG,city1);
-            StringTokenizer stringTokenizer = new StringTokenizer(city1,",");
-            int l=stringTokenizer.countTokens();
-            for (int i=0;i<l-2;i++)
-            {
-                city1=stringTokenizer.nextToken();
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        settingRequest();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "Connection Suspended!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Connection Failed!", Toast.LENGTH_SHORT).show();
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, 90000);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
             }
-            city=stringTokenizer.nextToken();
-            StringTokenizer stringTokenizer1=new StringTokenizer(city);
-            city=stringTokenizer1.nextToken();
-            city=city1+", "+city;
-            getForecast(latitude,longitude,city);
-        }catch(Exception e)
-        {
-
+        } else {
+            Log.i("Current Location", "Location services connection failed with code " + connectionResult.getErrorCode());
         }
+    }
 
+    /*Method to get the enable location settings dialog*/
+    public void settingRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);    // 10 seconds, in milliseconds
+        mLocationRequest.setFastestInterval(1000);   // 1 second, in milliseconds
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
+                        builder.build());
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+
+            @Override
+            public void onResult(@NonNull LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates state = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied. The client can
+                        // initialize location requests here.
+                        try {
+                            getLocation();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied, but this can be fixed
+                        // by showing the user a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(MainActivity.this, 1000);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way
+                        // to fix the settings so we won't show the dialog.
+                        break;
+                }
+            }
+
+        });
     }
 
     @Override
-    public void onProviderDisabled(String provider) {
-        Toast.makeText(MainActivity.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        final LocationSettingsStates states = LocationSettingsStates.fromIntent(data);
+        switch (requestCode) {
+            case 1000:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        // All required changes were successfully made
+                        try {
+                            getLocation();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        // The user was asked to change settings, but chose not to
+                        Toast.makeText(this, "Location Service not Enabled", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                break;
+        }
     }
 
+    public void getLocation() throws IOException {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        } else {
+            /*Getting the location after aquiring location service*/
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+
+            if (mLastLocation != null) {
+                try {
+                    Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                    List<android.location.Address> addresses = geocoder.getFromLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), 1);
+                    String city1 = addresses.get(0).getAddressLine(0);
+                    Log.d(TAG, city1);
+                    StringTokenizer stringTokenizer = new StringTokenizer(city1, ",");
+                    int l = stringTokenizer.countTokens();
+                    for (int i = 0; i < l - 2; i++) {
+                        city1 = stringTokenizer.nextToken();
+                    }
+                    city = stringTokenizer.nextToken();
+                    StringTokenizer stringTokenizer1 = new StringTokenizer(city);
+                    city = stringTokenizer1.nextToken();
+                    city = city1 + ", " + city;
+                    Log.i(TAG, "Rooney");
+                    latitude = mLastLocation.getLatitude();
+                    longitude = mLastLocation.getLongitude();
+                    Log.i(TAG, latitude + " - " + longitude);
+                }
+                catch (Exception e){}
+            } else {
+                /*if there is no last known location. Which means the device has no data for the loction currently.
+                 * So we will get the current location.
+                 * For this we'll implement Location Listener and override onLocationChanged*/
+                Log.i("Current Location", "No data for location found");
+
+                if (!mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, MainActivity.this);
+            }
+        }
+    }
+
+    /*When Location changes, this method get called. */
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        latitude=mLastLocation.getLatitude();
+        longitude=mLastLocation.getLongitude();
     }
 
-    @Override
-    public void onProviderEnabled(String provider) {
-    }
+
+
 }
+
+
+
